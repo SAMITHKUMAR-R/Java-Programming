7e166b4e713de6465dda445f0f3d07ce7390d11e
SAMITHKUMAR R <sr.samithkumar@gmail.com>
2026-01-28 17:41:52 +0530

    Add files via upload

---
+++ MarkSheet.java
@@ -0,0 +1,24 @@
+package programs;
+
+/*
+'John' studying in the government engineering college plans to create software to prepare the 
+mark sheet that contains the mark and grade details. He is instructed to prepare the software 
+using java language and use the method of Scanner class of UTIL package to receive the input 
+from the user. Display the details.
+*/
+
+import java.util.Scanner; 
+public class MarkSheet { 
+	public static void main(String[] args) { 
+		Scanner sc = new Scanner(System.in); 
+        // Getting input 
+        System.out.print("Enter Mark: "); 
+        double mark = sc.nextDouble(); 
+        System.out.print("Enter Grade: "); 
+        String grade = sc.next(); 
+        // Displaying output 
+        System.out.println("Mark : " + mark); 
+        System.out.println("Grade : " + grade); 
+        sc.close(); 
+	}
+} 
