943d9c798f6d5f4b401fa3b0f1a4122aa52430c1
SAMITHKUMAR R <sr.samithkumar@gmail.com>
2026-01-28 17:42:52 +0530

    Add files via upload

---
+++ OddEvenCheck.java
@@ -0,0 +1,26 @@
+package programs;
+
+/*
+  A group of students attending  the evening class in the school for learning programming 
+concepts. Get the total number of students and check whether  the total is odd or even. Guide 
+them  to find the type of given number  without using  % operator.
+*/
+
+import java.util.Scanner;
+
+public class OddEvenCheck { 
+	public static void main(String[] args) { 
+		Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter total number of students: "); 
+        int num = sc.nextInt(); 
+        // Check odd or even without using % operator 
+        int quotient = num / 2; 
+        int test = quotient * 2; 
+        if (test == num) { 
+        	System.out.println("The number is EVEN."); 
+        } else { 
+        	System.out.println("The number is ODD."); 
+        } 
+        sc.close(); 
+   }
+}
---
+++ ReverseOddPosition.java
@@ -0,0 +1,28 @@
+package programs;
+
+/*
+Get the string from user like ‘Java is Object Oriented Language’. Find the reverse of this string 
+and print it using string methods and display the elements present in the odd position. 
+ */
+
+import java.util.Scanner; 
+public class ReverseOddPosition { 
+	public static void main(String[] args) { 
+		try (Scanner sc = new Scanner(System.in)){ 
+			// Input 
+			System.out.println("Enter a string:"); 
+			String str = sc.nextLine(); 
+
+			// Reverse using StringBuilder 
+			String reversed = new StringBuilder(str).reverse().toString(); 
+			System.out.println("Reversed String: " + reversed); 
+			// Display characters at odd positions (index starts from 0) 
+			System.out.print("Characters at odd positions: "); 
+			for (int i = 0; i < str.length(); i++) { 
+				if (i % 2 != 0) {   // odd index 
+					System.out.print(str.charAt(i) + " "); 
+				} 
+			} 
+		} 
+	}
+}
