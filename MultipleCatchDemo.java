ce4dd4e0bf59e013acea4f9e3df9d8aa8b6da9a6
SAMITHKUMAR R <sr.samithkumar@gmail.com>
2026-01-29 18:30:57 +0530

    Update MultipleCatchDemo.java

--- MultipleCatchDemo.java
+++ MultipleCatchDemo.java
@@ -9,26 +9,23 @@ element then it would throw this exception.
 public class MultipleCatchDemo {
 	public static void main(String[] args) { 
 		try { 
-			// Array of size 6 
+		
 			int[] arr = new int[6]; 
-			// Storing value 30 in 8th position → index 7 
             arr[7] = 30;   
                } 
-        // 1st catch – Specific exception 
         catch (ArrayIndexOutOfBoundsException e) { 
         	System.out.println("Exception Caught: ArrayIndexOutOfBoundsException"); 
         } 
 		
-        // 2nd catch – Another specific exception 
         catch (NullPointerException e) { 
         	System.out.println("Exception Caught: NullPointerException"); 
         }
 		
-		// 3rd catch – General exception (catch-all) 
 		catch (Exception e) { 
 			System.out.println("Exception Caught: General Exception"); 
 		} 
 	} 
 } 
+
 
 
