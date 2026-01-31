e930f09dfa645f37e036bb74747416446d2870f3
SAMITHKUMAR R <sr.samithkumar@gmail.com>
2026-01-31 19:58:18 +0530

    Add files via upload

---
+++ EmailNameExtractor.java
@@ -0,0 +1,32 @@
+package programs;
+
+/*
+A company maintains employee email IDs in the format "name@company.com". 
+Write a program to extract the employee’s name part before "@". 
+Ensure the name is displayed in uppercase. 
+If the email does not contain "@", print "Invalid Email". 
+Use substring() and toUpperCase(). 
+Test with "karthik@tech.com". 
+Expected output: "KARTHIK". 
+ */
+
+import java.util.Scanner; 
+public class EmailNameExtractor { 
+	public static void main(String[] args) { 
+
+		try (Scanner sc = new Scanner(System.in)){ 
+			System.out.println("Enter email ID:"); 
+			String email = sc.nextLine(); 
+
+			// Check for "@" 
+			if (!email.contains("@")) { 
+				System.out.println("Invalid Email"); 
+			} 
+			else { 
+				int index = email.indexOf("@"); 
+				String name = email.substring(0, index);   // extract before "@" 
+				System.out.println(name.toUpperCase()); 
+			} 
+		} 
+	} 
+}
