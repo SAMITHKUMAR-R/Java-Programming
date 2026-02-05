48d8453625f3a68b5944e410e805b12c440c8077
SAMITHKUMAR R <sr.samithkumar@gmail.com>
2026-02-05 23:46:11 +0530

    Add files via upload

---
+++ ThreadPriorityDemo.java
@@ -0,0 +1,36 @@
+package programs;
+
+/*
+Write a Java program to create three threads and set priority to these threads in the following 
+ways: 
+• T1 – MIN_PRIORITY 
+• T2 – NORM_PRIORITY 
+• T3 – MAX_PRIORITY
+ */
+
+class MyThread extends Thread { 
+	public MyThread(String name) { 
+		super(name); 
+	} 
+	public void run() { 
+		System.out.println("Running: " + this.getName() + " | Priority: " + this.getPriority()); 
+	} 	
+}
+
+public class ThreadPriorityDemo { 
+	public static void main(String[] args) { 
+		MyThread T1 = new MyThread("Thread T1"); 
+		MyThread T2 = new MyThread("Thread T2"); 
+		MyThread T3 = new MyThread("Thread T3"); 
+		
+		// Setting priorities 
+		T1.setPriority(Thread.MIN_PRIORITY);   // Priority 1 
+		T2.setPriority(Thread.NORM_PRIORITY);  // Priority 5 
+		T3.setPriority(Thread.MAX_PRIORITY);   // Priority 10 
+		
+		// Start threads 
+		T1.start(); 
+		T2.start(); 
+		T3.start(); 
+	} 
+} 
