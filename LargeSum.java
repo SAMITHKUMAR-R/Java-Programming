ca6e3fd49e8c6b2df6b71c4d523e157ef3f6ca82
SAMITHKUMAR R <sr.samithkumar@gmail.com>
2026-02-25 23:55:11 +0530

    Update LargeSum.java

--- LargeSum.java
+++ LargeSum.java
@@ -4,27 +4,21 @@ import java.io.*;
 import java.util.*;
 import java.math.BigInteger;
 
-
 public class LargeSum {
-
 		public static void main(String[] args) {
 	        Scanner sc = new Scanner(System.in);
 	        if (!sc.hasNextInt()) return;
-	        
 	        int n = sc.nextInt();
 	        BigInteger sum = BigInteger.ZERO;
-
 	        for (int i = 0; i < n; i++) {
-	       
 	            String bigNumStr = sc.next();
-	           
 	            sum = sum.add(new BigInteger(bigNumStr));
 	        }
-
 	        String result = sum.toString();
 	        System.out.println(result.substring(0, 10));
 	    }
 
 	}
+
 
 
