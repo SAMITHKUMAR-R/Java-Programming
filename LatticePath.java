b4b75c6c0210cd3e8308617e9a9faf01d470214f
SAMITHKUMAR R <sr.samithkumar@gmail.com>
2026-02-25 23:51:02 +0530

    Update LatticePath.java

--- LatticePath.java
+++ LatticePath.java
@@ -4,21 +4,17 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-    
         long[][] dp = new long[501][501];
         int mod = 1000000007;
-
         for (int i = 0; i <= 500; i++) {
             dp[i][0] = 1;
             dp[0][i] = 1;
         }
-
         for (int i = 1; i <= 500; i++) {
             for (int j = 1; j <= 500; j++) {
                 dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % mod;
             }
         }
-
         while (t-- > 0) {
             int n = sc.nextInt();
             int m = sc.nextInt();
